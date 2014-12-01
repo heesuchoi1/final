36257edb12026f79608a877f02b45a5dd4773e1a
heesuchoi1 <heesu7563@gmail.com>
2014-12-01 17:18:46 +0900

    ds

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+var song;
+
 function setup(){
 	song = loadSound('soran-cheese.mp3');
 	createCanvas(800,800);
 	backgound(255);
 }
 
-function draw(){
-	
+function mousePressed(){
+	if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
 }

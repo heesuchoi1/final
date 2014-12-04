2742f13b6d820a8b08ef109a35a361ec3fff176f
heesuchoi1 <heesu7563@gmail.com>
2014-12-04 15:36:49 +0900

    w

--- sketch.js
+++ sketch.js
@@ -72,23 +72,23 @@ function drawSnow(){
 
   fill(255);
   ellipse(700,dir1,50,50);
-  ellipse(100,dir1+500,20,20);
+  ellipse(100,dir1+80,20,20);
   ellipse(200,dir1+r,30,30);
-  ellipse(300,dir1+800,25,25);
+  ellipse(300,dir1+90,25,25);
   ellipse(400,dir1+100,20,20);
-  ellipse(500,dir1+350,34,34);
+  ellipse(500,dir1+80,34,34);
   ellipse(600,dir1+270,47,47);
-  ellipse(800,dir1+900,25,25);
-  ellipse(900,dir1+670,34,34);
+  ellipse(800,dir1+180,25,25);
+  ellipse(900,dir1+210,34,34);
   ellipse(1000,dir1+230,17,17);
-  ellipse(1100,dir1+590,28,28);
-  ellipse(1200,dir1+312,33,33);
-  ellipse(1300,dir1+100,50,50);
-  ellipse(1400,dir1+400,27,27);
-  ellipse(1500,dir1+777,35,35);
+  ellipse(1100,dir1+130,28,28);
+  ellipse(1200,dir1+150,33,33);
+  ellipse(1300,dir1+80,50,50);
+  ellipse(1400,dir1+260,27,27);
+  ellipse(1500,dir1+200,35,35);
     dir1=dir1+3;     
-    if(dir1>1700){
-     dir1=-1700;
+    if(dir1>300){
+     dir1=-300;
  }
 
 }

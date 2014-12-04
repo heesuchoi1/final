1ae57e8eec7cc58e040acde4da4c40557a4f6433
heesuchoi1 <heesu7563@gmail.com>
2014-12-04 14:33:34 +0900

    ewt

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var song, analyzer;
+var r, g, b;
 
 function preload() {
   song = loadSound("./cheese.mp3");
@@ -9,13 +10,19 @@ function setup() {
   song.loop();
   analyzer = new p5.Amplitude();
   analyzer.setInput(song);
-
+    r = random(255);
+    g = random(255);
+    b = random(255);
 
 }
 
 function draw() {
   background(131,210,222);
   var vol = analyzer.getLevel();
+  strokeWeight(2);
+  stroke(255);
+  fill(r, g, b, 127);
+  ellipse(width/2, height/2, 120+vol*200, 120+vol*200)
   stroke(255);
   fill(Math.random()*255,Math.random()*255,Math.random()*255,100);
   ellipse(width/2, height/2, 90+vol*500, 90+vol*500);
@@ -23,5 +30,16 @@ function draw() {
   ellipse(width/2, height/2, 50+vol*200, 50+vol*200);
   fill(255,74,74);
   ellipse(width/2, height/2, 10+vol*200, 10+vol*200);
+
 }
 
+function mousePressed() {
+  // Check if mouse is inside the circle
+  var d = dist(mouseX, mouseY, 360, 200);
+  if (d < 300) {
+    // Pick new random color values
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+}

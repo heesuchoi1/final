c1201f1b9a64feffbfe6d3f636e832d6ecd31675
heesuchoi1 <heesu7563@gmail.com>
2014-12-04 14:04:00 +0900

    wetwer
    d

--- sketch.js
+++ sketch.js
@@ -1,25 +1,20 @@
 var song, analyzer;
 
-function setup() {
+function preload() {
   song = loadSound("cheese.mp3");
+}
+
+function setup() {
   createCanvas(710, 200);
   song.loop();
-
-  // create a new Amplitude analyzer
   analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
   analyzer.setInput(song);
 }
 
 function draw() {
   background(255);
-
-  // Get the overall volume (between 0 and 1.0)
   var vol = analyzer.getLevel();
   fill(127);
   stroke(0);
-
-  // Draw an ellipse with size based on volume
   ellipse(width/2, height/2, 10+vol*200, 10+vol*200);
 }

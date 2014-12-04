2be06bd7cc14d8f6c9b77524709881ca5435a804
heesuchoi1 <heesu7563@gmail.com>
2014-12-04 14:24:55 +0900

    ewetw34234

--- sketch.js
+++ sketch.js
@@ -9,14 +9,17 @@ function setup() {
   song.loop();
   analyzer = new p5.Amplitude();
   analyzer.setInput(song);
+
+
 }
 
 function draw() {
-  background(255);
+  background(131,210,222);
   var vol = analyzer.getLevel();
   fill(255,74,74);
   stroke(255);
-  ellipse(100, 150, 10+vol*200, 10+vol*200,10);
+  ellipse(100, 150, 10+vol*200, 10+vol*200);
   fill(182,239,44);
-  rect(200, 150, vol*200, 50+vol*200,80);
+  ellipse(200, 150, vol*200, 50+vol*200);
+  fill(Math.random()*255,Math.random()*255,Math.random()*255,100);
 }
